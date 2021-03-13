6dbabc97c459a9e57841982782adda10617b137d
TrungYU <quoctrung1220@gmail.com>
2021-03-13 13:40:02 -0800

    added bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       hello world
+      <Button variant="primary">Primary</Button>{' '}
     </div>
   );
 }
