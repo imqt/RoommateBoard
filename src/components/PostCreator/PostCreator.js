1fd39829311dc8d36d9fb76084a95b76d9010d3a
TrungYU <quoctrung1220@gmail.com>
2021-03-13 17:22:57 -0800

    switched postcreator to using react quill instead of editor js

--- src/components/PostCreator/PostCreator.js
+++ src/components/PostCreator/PostCreator.js
@@ -3,13 +3,16 @@ import EditorJS from '@editorjs/editorjs';
 import Header from '@editorjs/header'; 
 import SimpleImage from '@editorjs/simple-image'; 
 import List from '@editorjs/list';
+import ReactQuill from 'react-quill'; // ES6
+
 
 function PostCreator(props) {
+    console.log("Post creator rerendered");
     
     const editor = new EditorJS({ 
 
         holder: 'editorjs', 
-    
+        placeholder: 'Start writing something!',
         tools: { 
             image: {
                 class: SimpleImage,
@@ -44,11 +47,30 @@ function PostCreator(props) {
         </Modal.Header>
         
         <Modal.Body>
-            <div style={{borderWidth:"2px", borderColor:"black",borderStyle:"solid"}} id="editorjs"></div>        
+            <ReactQuill 
+                theme="snow"
+                modules = {{
+                    toolbar: [
+                      [{ 'header': [1, 2, false] }],
+                      ['bold', 'italic', 'underline','strike', 'blockquote'],
+                      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+                      ['link', 'image'],
+                      ['clean']
+                    ],
+                  }}
+                  />
+            {/* <div style={{borderWidth:"2px", borderColor:"black",borderStyle:"solid"}} id="editorjs"></div>    */}
         </Modal.Body>
 
         <Modal.Footer>
-            <Button onClick={() => {console.log("Grab and send data")}}>Post</Button>
+            <Button onClick={() => {
+                editor.save()
+                .then((savedData) => {
+                    console.log("AAA" + savedData);
+                });
+            }}
+            >Post
+            </Button>
             <Button onClick={props.onHide}>Close</Button>
         </Modal.Footer>
 
