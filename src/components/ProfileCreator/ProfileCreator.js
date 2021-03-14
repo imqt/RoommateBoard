72f0ee975e31ff4dc191329e0fc78a3f38a272e9
michael <michael@box>
2021-03-13 18:48:25 -0800

    profile

--- src/components/ProfileCreator/ProfileCreator.js
+++ src/components/ProfileCreator/ProfileCreator.js
@@ -1,4 +1,6 @@
+import styles from './ProfileCreator.css';
 import { Modal, Button } from 'react-bootstrap';
+import ProfileImg from '../../components/img/profileImage.jpeg';
 
 function ProfileCreator(props) {
   return (
@@ -10,34 +12,42 @@ function ProfileCreator(props) {
     >
       <Modal.Header closeButton={true}>
         <Modal.Title id="contained-modal-title-vcenter">
-          Create a profile
+               Create a profile
         </Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
-        <h4>Name:</h4>
-        <input class="form-control" type="text" placeholder="Name"></input>
+        <div class="singleLineForm">
+          <h4>Name:</h4>
+          <input class="form-control" type="text" placeholder="Name"></input>
+        </div>
         {/* <textarea name="" id="" cols="90" rows="10"></textarea> */}
-        <h4>Gender:</h4>
-        <input type="radio" id="male" name="gender" value="male"></input>
-        <label for="male">Male</label><br />
-        <input type="radio" id="female" name="gender" value="female"></input>
-        <label for="female">Female</label><br />
-        <input type="radio" id="other" name="gender" value="other"></input>
-        <label for="other">Other</label>
-        <h4>Pets:</h4>
-        <input type="radio" id="male" name="gender" value="male"></input>
-        <label for="male">Yes</label><br />
-        <input type="radio" id="female" name="gender" value="female"></input>
-        <label for="female">No</label><br />
-        <h4>Price:</h4>
-        <select class="form-select form-select-sm" aria-label=".form-select-sm example">
-          <option selected>Price: </option>
-          <option value="1">&#62; 500</option>
-          <option value="2">500 ~ 750</option>
-          <option value="3">751 ~ 1000</option>
-          <option value="3">1001 ~ 1500</option>
-        </select>
+        <div class="singleLineForm">
+          <h4>Gender:</h4>
+          <input type="radio" id="male" name="gender" value="male"></input>
+          <label for="male">Male</label><br />
+          <input type="radio" id="female" name="gender" value="female"></input>
+          <label for="female">Female</label><br />
+          <input type="radio" id="other" name="gender" value="other"></input>
+          <label for="other">Other</label>
+        </div>
+        <div class="singleLineForm">
+          <h4>Pets:</h4>
+          <input type="radio" id="male" name="gender" value="male"></input>
+          <label for="male">Yes</label><br />
+          <input type="radio" id="female" name="gender" value="female"></input>
+          <label for="female">No</label><br />
+        </div>
+        <div class="singleLineForm">
+          <h4>Price:</h4>
+          <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+            <option selected>Price: </option>
+            <option value="1">&#62; 500</option>
+            <option value="2">500 ~ 750</option>
+            <option value="3">751 ~ 1000</option>
+            <option value="3">1001 ~ 1500</option>
+          </select>
+        </div>
         <div class="form-group">
           <label for="exampleFormControlTextarea1"><h4>Description:</h4></label>
           <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
