a6da9b0cd12902c862c79b5922ac812fe2c68d77
TrungYU <quoctrung1220@gmail.com>
2021-03-13 16:13:27 -0800

    adding editorjs tools

--- src/components/PostCreator/PostCreator.js
+++ src/components/PostCreator/PostCreator.js
@@ -1,31 +1,59 @@
 import { Modal, Button} from 'react-bootstrap';
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header'; 
+import SimpleImage from '@editorjs/simple-image'; 
+import List from '@editorjs/list';
 
 function PostCreator(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton={true}>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Create a post
-        </Modal.Title>
-      </Modal.Header>
+    
+    const editor = new EditorJS({ 
+
+        holder: 'editorjs', 
+    
+        tools: { 
+            image: {
+                class: SimpleImage,
+                inlineToolbar: ['link'],
+            },
+            header: {
+                class: Header,
+                config: {
+                    placeholder: 'Enter a header',
+                    levels: [3, 4, 5, 6],
+                    defaultLevel: 5,
+                }
+            },
+            list: {
+                class: List,
+                inlineToolbar: true,
+            },
+        }, 
+    })
       
-      <Modal.Body>
-        <h4>Title:</h4>
-        <textarea name="" id="" cols="90" rows="10"></textarea>
-      </Modal.Body>
+      return (
+        <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        >
+        <Modal.Header closeButton={true}>
+            <Modal.Title id="contained-modal-title-vcenter">
+            Create a post
+            </Modal.Title>
+        </Modal.Header>
+        
+        <Modal.Body>
+            <div style={{borderWidth:"2px", borderColor:"black",borderStyle:"solid"}} id="editorjs"></div>        
+        </Modal.Body>
 
-      <Modal.Footer>
-        <Button onClick={() => {console.log("Grab and send data")}}>Post</Button>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
+        <Modal.Footer>
+            <Button onClick={() => {console.log("Grab and send data")}}>Post</Button>
+            <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
 
-    </Modal>
-  );
+        </Modal>
+    );
 }
 
 export default PostCreator;
